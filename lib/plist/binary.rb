d17ede3046ad66cc9a96a4ac18383bd607add28d
Raels Koder <raels@uniblab.com>
2014-02-02 19:28:48 -0800

    added more debugging to show plist errors as ints

--- lib/plist/binary.rb
+++ lib/plist/binary.rb
@@ -334,7 +334,7 @@ module Plist
     end
 
     def self.decode_binary_plist_obj(plist, offset, ref_byte_size)
-	  pp ['plist', plist, 'offset', offset, 'ref_byte_size', ref_byte_size ]
+	  pp ['plist', plist, 'offset', offset, 'ref_byte_size', ref_byte_size, "plist[offset]", plist[offset].to_i ]
       case plist[offset]
       when CFBinaryPlistMarkerASCIIString..(CFBinaryPlistMarkerASCIIString | 0xf)
         length, offset = decode_length(plist, offset)
